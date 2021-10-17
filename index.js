e1c8fb2ccbf67f0eb8cada08b51446c9b74a5ca3
plaboncosta <plabonjoseph@gmail.com>
2021-10-17 15:53:38 +0600

    make service port dynamic

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000; 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -9,3 +9,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
